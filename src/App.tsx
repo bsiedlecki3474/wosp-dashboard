c4be9f6f0021e33ef864bbc51322aa7aebfc9947
Bartosz Siedlecki <bsiedlecki3474@gmail.com>
2024-11-30 15:34:27 +0100

    Add user access verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,51 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 import { app } from './firebase'
 import { signInWithPopup, signOut, getAuth, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
-// import { useAuthState } from ''
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { getDatabase, ref, get, set, push, query, orderByChild, equalTo } from 'firebase/database';
+import type { User } from 'firebase/auth'
 
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
+const db = getDatabase(app);
 
+const isUserAdmin = async (user: User): Promise<boolean> => {
+  if (!user?.uid) return false;
+  const usersRef = ref(db, `tenants/${user.uid}/users`);
+  const readUsers = await get(query(usersRef, orderByChild('uid'), equalTo('mPf1iBnjH8XSgLQ1SNUVLVs4vp43')));
+  return readUsers.val().filter((el: User) => !!el?.uid).length > 0;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isAdmin, setIsAdmin] = useState(false);
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    user && isUserAdmin(user).then((result) => setIsAdmin(result));
+  }, [user])
 
   if (loading) {
     return <p>Loading...</p>
   } else if (!user) {
     return (
       <>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <p>testing stuff</p>
         <button onClick={() => signInWithPopup(auth, provider)}>login</button>
       </>
     )
   }
 
-  console.log(user.displayName);
-  // console.log(import.meta.env)
-
   return (
-    <button onClick={() => signOut(auth)}>logout</button>
+    <div>
+      <p>displayName: <code>{user.displayName}</code></p>
+      <p>email: <code>{user.email}</code></p>
+      <p>isAdmin: <code>{isAdmin.toString()}</code></p>
+      <button onClick={() => signOut(auth)}>logout</button>
+    </div>
   )
-
-
-
-
-
-
 }
 
 export default App
